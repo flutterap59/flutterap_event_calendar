bf364e4164b3729d2da2871b590a03308c4b2043
hanieh hojjatzadeh <haniehhojjatzadeh@gmail.com>
2023-07-06 12:57:30 +0330

    version 1.0.4

--- example/main.dart
+++ example/main.dart
@@ -13,13 +13,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SizedBox(
 
             height: 700,
-            child: FxGeogorianEventCalendar()),
+            child: FxGeogorianEventCalendar(
+              onViewChanged: (details){}
+
+            )),
       ),
     );
   }
